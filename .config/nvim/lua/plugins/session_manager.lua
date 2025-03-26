61c7e1037befd93263cf19776c4b5cdd990839eb
McSnobby <erik.grenner@icloud.com>
2025-03-26 20:03:32 +0100

    load sessions

--- .config/nvim/lua/plugins/session_manager.lua
+++ .config/nvim/lua/plugins/session_manager.lua
@@ -5,13 +5,16 @@ return {
             "nvim-lua/plenary.nvim"
         },
         lazy = false,
-        config = function ()
-            local session_config =  require("session_manager.config")
+        keys = {
+            { "<leader>ws", "<cmd>SessionManager save_current_session<CR>" },
+            { "<leader>ds", "<cmd>SessionManager delete_current_dir_session<CR>" },
+            { "<leader>ls", "<cmd>SessionManager load_session<CR>" }
+        },
+        config = function()
+            local session_config = require("session_manager.config")
             require("session_manager").setup({
                 autoload_mode = { session_config.AutoloadMode.CurrentDir, session_config.AutoloadMode.LastSession }
             })
-            vim.keymap.set("n", "<leader>ws", ":SessionManager save_current_session<CR>")
-            vim.keymap.set("n", "<leader>ds", ":SessionManager delete_current_dir_session<CR>")
         end
     }
 }
